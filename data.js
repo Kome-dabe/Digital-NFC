55277e99d30223683902bf15dfa45b5dd57466b3
kome <gluttonynecrozma@gmail.com>
2026-08-10 11:27:20 +0800

    Initial commit

---
+++ data.js
@@ -0,0 +1,39 @@
+/* ======================================================
+   ONE ENTRY PER PERSON. To add someone new:
+   1. Add their designed card PNG to img/  (e.g. img/NEWID-card.png)
+   2. Add their headshot PNG to img/       (e.g. img/NEWID-profile.png)
+   3. Copy one of the blocks below, change the key (e.g. "NEWID") and fields.
+   Their link is then: yoursite.github.io/my-calling-card/#NEWID
+   ====================================================== */
+   window.CARDS = {
+
+    JDP: {
+        cardImage: "img/Calling-Card-JDP.png",
+        profilePhoto: "img/Profile-JDP.png",
+        firstName: "John Derek",
+        lastName: "Porciuncula",
+        title: "Department Manager III",
+        department: "Internal Audit Service",
+        company: "Philippine Charity Sweepstakes Office",
+        phones: ["+63 919 065 8734", "(02) 8706-3942"],
+        email: "jdporciuncula@pcso.gov.ph",
+        website: "https://www.pcso.gov.ph",
+        address: "9F Sun Plaza Bldg., 1507 Shaw Blvd. cor. Princeton St., Mandaluyong City, 1552"
+    }
+
+    // Add the next person here, e.g.:
+    // MARIA: {
+    //     cardImage: "img/MARIA-card.png",
+    //     profilePhoto: "img/MARIA-profile.png",
+    //     firstName: "Maria",
+    //     lastName: "Santos",
+    //     title: "...",
+    //     department: "...",
+    //     company: "Philippine Charity Sweepstakes Office",
+    //     phones: ["..."],
+    //     email: "...",
+    //     website: "https://www.pcso.gov.ph",
+    //     address: "..."
+    // }
+
+};
